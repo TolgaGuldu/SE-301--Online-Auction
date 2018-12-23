fe373a25eff133481292b94c8de1a7d30aadf989
Maskoliver <33281901+Maskoliver@users.noreply.github.com>
2018-12-23 22:36:14 +0300

    DELETE

--- Alfa APP/MyApp/AuctionApp_server/test/services/auctions.test.js
+++
@@ -1,8 +0,0 @@
-const app = require('../../src/app');
-
-describe('\'auctions\' service', () => {
-  it('registered the service', () => {
-    const service = app.service('auctions');
-    expect(service).toBeTruthy();
-  });
-});
